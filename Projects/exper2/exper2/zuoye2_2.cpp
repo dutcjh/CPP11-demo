74d8a6a8b57921e976ea38c18bc35c65e264283d
dutcjh <caschenjianhui@gmail.com>
2017-02-28 09:46:49 +0800

    zuoye2

---
+++ Projects/exper2/exper2/zuoye2_2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	/*unsigned int x = 015, y = 0x2b;
+	cout << "x|y  " << (x | y) << endl;
+	cout << "x^y  " << (x^y) << endl;
+	cout << "x&y  " << (x&y) << endl;
+	cout << "~x+~y  " << (~x + ~y) << endl;*/
+	//int i = 10, j = 5;
+	//cout << "~i^j  " << (~i^j) << endl;
+	//cout << "i = " << i << "   j = " << j << endl;
+	/*cout << "1、 " << (3 + 2 << 1 + 1) << endl;
+	cout << "2、 " << (2 * 9 | 3 << 1) << endl;
+	cout << "3、 " << (5 % -3 * 2 / 6 - 3) << endl;
+	cout << "4、 " << (8 == 3 <= 2 & 5) << endl;*/
+	char c1 = 'C', c2 = '+', c3 = '+';
+	cout << "I say:\"" << c1 << c2 << c3 << '\"';
+	cout << "\t\t" << "He says:\"C++ is very interesting!\"" << '\n';
+
+	return 0;
+}
---
+++ cpp1/exci2_2_1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    //int a = 5, b = 3;
+    //int c = 0;
+    //cout << "1、" << (++b,a=10,a+5);
+    //cout << "2、" << a||b+4&&a*b;
+    //cout << "3、" << a=1,b=2,a>b?++a:++b;
+    //cout << "4、" << !a&&b++;
+    //cout << endl;
+    //cout << "a = " << a << endl;
+    //cout << "b = " << b << endl;
+    cout << "1、 " << (3 + 2 << 1 + 1) << endl;
+	cout << "2、 " << (2 * 9 | 3 << 1) << endl;
+	cout << "3、 " << (5 % -3 * 2 / 6 - 3) << endl;
+	cout << "4、 " << (8 == 3 <= 2 & 5) << endl;
+    return 0;
+}
